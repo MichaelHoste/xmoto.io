4a630af0180cc1bc48f7e89e1092ca805e30f4ac
Michaël Hoste <michael.hoste@gmail.com>
2014-02-15 20:59:37 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XmotoIo::Application.config.secret_key_base = '04334cc3dbfa3aab21334898ca55628e367343a3ea6da253cb46d8cd19128ea8320bff149eaf5eac4602ff32f2d29a07e1ddc0cb8db3c3ffa64a44000958a4b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XmotoIo::Application.config.session_store :cookie_store, key: '_xmoto_io_session'
