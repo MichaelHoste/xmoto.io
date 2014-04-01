d8044d4b0dbad44295918eb61413d534c8d0e271
Michaël Hoste <michael.hoste@gmail.com>
2014-04-02 00:06:30 +0200

    provider_ignores_state for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
-           :scope            => ENV['FACEBOOK_SCOPE'],
-           :secure_image_url => true
+           :scope                  => ENV['FACEBOOK_SCOPE'],
+           :provider_ignores_state => true,
+           :secure_image_url       => true
 end
